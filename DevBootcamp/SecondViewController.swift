5c2f6c1c23b075fdc3e10652bc01848bbda63e93
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-25 15:43:10 +0100

    Initial Commit

---
+++ DevBootcamp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DevBootcamp
+//
+//  Created by Vyacheslav Horbach on 25/03/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
