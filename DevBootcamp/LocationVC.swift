c7bf4521d2fa245efb7279c91a6c8fce4e914ea1
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-25 16:47:39 +0100

    added a functions to update the map when user location changes + show it with suitable zoom out level

--- DevBootcamp/LocationVC.swift
+++ DevBootcamp/LocationVC.swift
@@ -14,6 +14,8 @@ class LocationVC: UIViewController, UITableViewDataSource, UITableViewDelegate, 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var map: MKMapView!
     
+    let regionRadius: CLLocationDistance = 1000
+    
     let locationManager = CLLocationManager()
     
     
@@ -52,6 +54,17 @@ class LocationVC: UIViewController, UITableViewDataSource, UITableViewDelegate, 
             map.showsUserLocation = true
         } else {
             locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2, regionRadius * 2)
+        map.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        if let loc = userLocation.location {
+            centerMapOnLocation(loc)
         }
     }
 }
